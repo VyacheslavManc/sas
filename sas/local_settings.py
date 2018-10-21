e3e2d5dad9249ea2afab945e78a58c7c05ccbcb8
VyacheslavManc <vyacheslavmancurov@gmail.com>
2018-10-21 20:04:05 +0300

    first commit

---
+++ sas/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sas_db',
+        'USER': 'sas_dba',
+        'PASSWORD': 'LeprikonSgorshochkomZolota',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
